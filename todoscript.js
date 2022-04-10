e5f5d669b167f8a1ecf83b20c299f92438621a98
Luis Rivas Lopez <76708966+Lrivaslo5@users.noreply.github.com>
2022-04-09 23:26:33 -0400

    Added Visual change to checked list items

--- todoscript.js
+++ todoscript.js
@@ -2,23 +2,30 @@
 var myNodelist = document.getElementsByTagName("li");
 
 for (var i = 0; i < myNodelist.length; i++) {
-    console.log(myNodelist);
     var span = document.createElement("SPAN");
     var text = document.createTextNode("\u00D7");
     span.className = "close";
     span.appendChild(text);
     myNodelist[i].appendChild(span);
-    console.log('poop');
 
 }
 
 // Click on close button to hide the current list item
-var close = document.getElementsByName("close");
-var index;
-for (let index = 0; index < close.length; index++) {
-    close[index].onclick = function() {
-        var div = this.parentElement;
-        div.style.display = "none";
+var close = document.getElementsByClassName("close");
 
+for (var index = 0; index < close.length; index++) {
+    close[index].onclick = function() {
+        var div = this.parentNode;
+        div.style.display = "none";
     }  
 }
+
+// Adding visual change for checked list items
+
+var list = document.querySelector('ul');
+list.addEventListener('click', function(ev) {
+    console.log(ev.target.tagName);
+    if( ev.target.tagName === 'LI') {
+        ev.target.classList.toggle('done');
+    }
+}, false);
