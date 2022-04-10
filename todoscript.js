d1c2efc8b5f5408d806409cad58719ac1cfac4e9
Luis Rivas Lopez <76708966+Lrivaslo5@users.noreply.github.com>
2022-04-09 23:04:37 -0400

    Debugged  "close" button to appear properly and added onclick event

--- todoscript.js
+++ todoscript.js
@@ -1,14 +1,24 @@
 // Create a close button and appending it to eack list item
+var myNodelist = document.getElementsByTagName("li");
 
-var myNodelist = document.getElementsByTagName("LI");
-var i;
-
-for (i = 0; i < myNodelist.length; i++){
+for (var i = 0; i < myNodelist.length; i++) {
+    console.log(myNodelist);
     var span = document.createElement("SPAN");
-    var text = document.createTextNode('u00D7');
+    var text = document.createTextNode("\u00D7");
     span.className = "close";
-    span.appendChild(txt);
+    span.appendChild(text);
     myNodelist[i].appendChild(span);
+    console.log('poop');
+
 }
 
 // Click on close button to hide the current list item
+var close = document.getElementsByName("close");
+var index;
+for (let index = 0; index < close.length; index++) {
+    close[index].onclick = function() {
+        var div = this.parentElement;
+        div.style.display = "none";
+
+    }  
+}
